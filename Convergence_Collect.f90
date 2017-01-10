aca3a0d20edfb92dfc42503e7398241933d156b1
Brandon Thornton <brandonthornton10@gmail.com>
2017-01-10 15:18:44 -0700

    First Iteration Poynting Magnitudes
    Sums the Poynting vector magnitudes of each point in an input-specified
    recti-formed space. The output is the final sum.
    
    Accounting for three dimensions allows for the greatest utility. In
    order to accomodate 1- or 2-dimensional simulations, set D = 1 or D = 2
    and set to 0 the unsimulated input field-arguments.
    
    Note that currently, the function computes the vector magnitudes of the
    Simulation-points, not the Evaluation-points, which would be averages of
    the Simulation-points.

---
+++ Convergence_Collect.f90
@@ -0,0 +1,51 @@
+function Convergence_Collect(D, Ex, Ey, Ez, Hx, Hy, Hz, &
+                             i_start, i_end, j_start, j_end, k_start, k_end) result(P_sum)
+double precision, intent(in) :: Ex, Ey, Ez, Hx, Hy, Hz
+integer, intent(in) :: D, i_start, i_end, j_start, j_end, k_start, k_end
+double precision, intent(out) :: P_sum
+double precision :: Px, Py, Pz
+integer :: i,j,k
+
+P_sum = 0.0
+Px = 0.0
+Py = 0.0
+Pz = 0.0
+
+if(D == 3)then !add Poynting magnitudes in 3D
+ do i = i_start,i_end
+  do j = j_start,j_end
+   do k = k_start,k_end
+    Px = Ey(i,j,k)*Hz(i,j,k) - Ez(i,j,k)*Hy(i,j,k)
+    Py = Ez(i,j,k)*Hx(i,j,k) - Ex(i,j,k)*Hz(i,j,k)
+    Pz = Ex(i,j,k)*Hy(i,j,k) - Ey(i,j,k)*Hx(i,j,k)
+    P_sum = P_sum + sqrt(Px**2 + Py**2 + Pz**2)
+   enddo
+  enddo
+ enddo
+endif
+
+if(D == 2)then !add Poynting magnitudes in 2D
+ do i = i_start,i_end
+  do j = j_start,j_end
+   Px = Ey(i,j)*Hz(i,j) - Ez(i,j)*Hy(i,j)
+   Py = Ez(i,j)*Hx(i,j) - Ex(i,j)*Hz(i,j)
+   Pz = Ex(i,j)*Hy(i,j) - Ey(i,j)*Hx(i,j)
+   P_sum = P_sum + sqrt(Px**2 + Py**2 + Pz**2) 
+  enddo
+ enddo
+endif
+
+if(D == 1)then !add Poynting magnitudes in 1D
+ do i = i_start,i_end
+  Px = Ey(i)*Hz(i) - Ez(i)*Hy(i)
+  Py = Ez(i)*Hx(i) - Ex(i)*Hz(i)
+  Pz = Ex(i)*Hy(i) - Ey(i)*Hx(i)
+  P_sum = P_sum + sqrt(Px**2 + Py**2 + Pz**2)
+ enddo
+endif
+ 
+if( D >= 4.or.D <= 0 )then !returns a marked P_sum if dimension is non-Euclidean
+ P_sum = -1.0
+endif
+
+end function Convergence_Collect
