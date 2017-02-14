e9dbf9b57ed96b425e3aee3cd998bfbc41dfd4c2
Brandon Thornton <brandonthornton10@gmail.com>
2017-02-13 21:20:33 -0700

    Change to abs for Detection
    Previous run produced the dipole, as desired. AND PRODUCED A RELATIVE
    ERROR FILE. Will seek Grid-Return for n == Nt for further confirmation
    of the elimination of NaNs. I also sillily had myrank projecting P_sum,
    when the detecting processor is actually nprocs/2.

--- Convergence_Vacuum.f90
+++ Convergence_Vacuum.f90
@@ -614,7 +614,7 @@ if(myrank == (nprocs)/2)then
     Hz(i,j) = Hz(i,j) + pulse(n)
 !   endif
 !   if(x(i) == x_detect .and. y(j) == y_detect)then
-    P_sum = P_sum + (Hz(i,j) + Hz(i-1,j) + Hz(i,j-1) + Hz(i-1,j-1) )/4.0
+    P_sum = P_sum + abs((Hz(i,j) + Hz(i-1,j) + Hz(i,j-1) + Hz(i-1,j-1) )/4.0)
 !   endif
 !  enddo
 ! enddo
@@ -898,7 +898,7 @@ enddo !Nt
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !.:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:.
 
-if(myrank == 0)then
+if(myrank == nprocs/2)then
  write(*,*) "P_sum = ", P_sum
  WRITE(*,*)"done time-stepping"
 endif
